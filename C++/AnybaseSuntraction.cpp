b98f7724863befe36f5a5aff398e85453810a129
Krish <Krishkumer150@gamil.com>
2024-05-04 12:52:39 +0530

    Array

--- C++/AnybaseSuntraction.cpp
+++ C++/AnybaseSuntraction.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
+<<<<<<< HEAD:C++/AnybaseSuntraction.cpp
+<<<<<<<< HEAD:C++/AnybaseSuntraction.cpp
+=======
+>>>>>>> 02c10cc (Array):Hello-World/C++/AnybaseSuntraction.cpp
 int anybaseSubtraction(){
 
 }
@@ -7,5 +11,30 @@ int main()
 {
     
 
+<<<<<<< HEAD:C++/AnybaseSuntraction.cpp
+========
+int main()
+{
+    // for (int a = 0; a <= 10; a++)
+    // {
+    //     cout << a << endl;
+    // }
+    int age = 88;
+
+    if (age <= 18)
+    {
+        cout << "you can go to school" << endl;
+    }
+    if (age >= 18 && age <= 30)
+    {
+        cout << "you can go to college" << endl;
+    }
+    else
+    {
+        cout << "You are eligible for marriage or Job" << endl;
+    }
+>>>>>>>> 02c10cc (Array):Hello-World/C++/HelloWorld.cpp
+=======
+>>>>>>> 02c10cc (Array):Hello-World/C++/AnybaseSuntraction.cpp
     return 0;
 }
